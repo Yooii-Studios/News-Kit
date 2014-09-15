d65840a8c012106eb6fd37b6d96a01c5269617fd
Wooseong Kim <kwosu87@me.com>
2014-09-15 20:17:38 +0900

    태블릿에서 메인 아이템들 사이즈 조금 더 수정. 바텀 2줄과 조금 더 보이게 수정함

--- News-L/src/main/java/com/yooiistudios/news/ui/adapter/MainBottomAdapter.java
+++ News-L/src/main/java/com/yooiistudios/news/ui/adapter/MainBottomAdapter.java
@@ -224,7 +224,7 @@ public class MainBottomAdapter extends
     }
 
     public static float getRowHeight(float width) {
-        return width * 3.0f / 4.0f;
+        return width * 3.3f / 4.0f;
     }
 
     @Override
