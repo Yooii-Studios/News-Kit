dd6d40437786e519503fe8bda050dcf984a8d37b
Wooseong Kim <kwosu87@me.com>
2014-09-15 11:59:48 +0900

    액션바 액티비티 추가

---
+++ News-L/src/main/java/com/yooiistudios/news/ui/activity/SettingActivity.java
@@ -0,0 +1,40 @@
+package com.yooiistudios.news.ui.activity;
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.yooiistudios.news.R;
+
+public class SettingActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_setting, container, false);
+            return rootView;
+        }
+    }
+}
