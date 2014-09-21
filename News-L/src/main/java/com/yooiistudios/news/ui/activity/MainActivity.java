c8fe8a6cf79add0b9071e9c146e4ea2cc2916244
Chris Jeong <crowjdh@gmail.com>
2014-09-21 10:41:59 +0900

    [Deliver #79226148] 메인 onResume에서 상태 체크, 핸들러 시작하도록 변경

--- News-L/src/main/java/com/yooiistudios/news/ui/activity/MainActivity.java
+++ News-L/src/main/java/com/yooiistudios/news/ui/activity/MainActivity.java
@@ -137,6 +137,7 @@ public class MainActivity extends Activity
     @Override
     protected void onResume() {
         super.onResume();
+        startNewsAutoRefreshIfReady();
     }
 
     @Override
