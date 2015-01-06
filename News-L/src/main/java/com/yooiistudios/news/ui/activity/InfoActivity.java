44df22ffc539d2b94a5f047f3b146c44cf2c8280
Wooseong Kim <kwosu87@me.com>
2015-01-06 19:12:41 +0900

    [Deliver #82189198] 인포탭 배너 id 적용하기

--- News-L/src/main/java/com/yooiistudios/news/ui/activity/InfoActivity.java
+++ News-L/src/main/java/com/yooiistudios/news/ui/activity/InfoActivity.java
@@ -2,14 +2,40 @@ package com.yooiistudios.news.ui.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
 import com.yooiistudios.news.R;
+import com.yooiistudios.news.iab.IabProducts;
+import com.yooiistudios.news.ui.adapter.SettingAdapter;
 
 public class InfoActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // OS에 의해서 kill 당할 경우 복구하지 말고 메인 액티비티를 새로 띄워줌
+        if (savedInstanceState != null) {
+            finish();
+            return;
+        }
         setContentView(R.layout.activity_info);
+    }
+
+    private void initAdView() {
+        // NO_ADS 만 체크해도 풀버전까지 체크됨
+        if (IabProducts.containsSku(getActivity().getApplicationContext(), IabProducts.SKU_NO_ADS)) {
+            mAdView.setVisibility(View.GONE);
+        } else {
+            mAdView.setVisibility(View.VISIBLE);
+            AdRequest adRequest = new AdRequest.Builder().build();
+            mAdView.loadAd(adRequest);
+        }
+    }
+
+    private void initListView() {
+        mSettingAdapter = new SettingAdapter(getActivity().getApplicationContext());
+        mListView.setAdapter(mSettingAdapter);
+        mListView.setOnItemClickListener(this);
     }
 }
