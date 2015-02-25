bcb04c091b9d5f96723b86db9c721d256ac53700
Chris Jeong <crowjdh@gmail.com>
2015-02-25 19:30:51 +0900

    롤리팝 이전 버전에서도 필터 애니메이션 되도록 구현

--- NewsFlow/src/main/java/com/yooiistudios/newsflow/ui/animation/ImageFilterAnimator.java
+++ NewsFlow/src/main/java/com/yooiistudios/newsflow/ui/animation/ImageFilterAnimator.java
@@ -4,6 +4,7 @@ import android.animation.ArgbEvaluator;
 import android.animation.ObjectAnimator;
 import android.animation.ValueAnimator;
 import android.annotation.TargetApi;
+import android.graphics.PorterDuff;
 import android.os.Build;
 import android.widget.ImageView;
 
@@ -21,11 +22,21 @@ public class ImageFilterAnimator {
     // DOUBT 파라미터가 많은데 팩토리로 빼야 하나?
     public static void animate(final ImageView imageView, int startArgb,
                                int endArgb, long duration) {
-        imageView.setColorFilter(startArgb);
-        ObjectAnimator animator;
+        ValueAnimator animator;
         if (Device.hasLollipop()) {
-            animator = createObjectAnimator(imageView.getColorFilter(), PROPERTY,
-                    endArgb);
+            imageView.setColorFilter(startArgb);
+            animator = createObjectAnimator(imageView.getColorFilter(), PROPERTY, endArgb);
+//            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+//
+//                @Override
+//                public void onAnimationUpdate(ValueAnimator animation) {
+//                    updateColorFilter(imageView);
+//                }
+//            });
+            animator.addUpdateListener(ColorFilterListener.create(imageView));
+
+
+
 //            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 //
 //                @Override
@@ -35,39 +46,47 @@ public class ImageFilterAnimator {
 //            });
 //            animator.setDuration(duration).start();
         } else {
-            animator =
-                    createObjectAnimatorBeforeLollipop(imageView.getColorFilter(), PROPERTY,
-                            endArgb);
+//            animator = createObjectAnimatorBeforeLollipop(imageView.getColorFilter(), PROPERTY,
+//                            endArgb);
+            imageView.getDrawable().setColorFilter(startArgb, PorterDuff.Mode.SRC_ATOP);
+
+            animator = ValueAnimator.ofObject(new ArgbEvaluator(), startArgb, endArgb);
+//            animator = ObjectAnimator.ofObject(
+//                    new Object() {
+//
+//                        private void setTestProperty(int argb) {
+//                            NLLog.i("setTestProperty", "argb : " + argb);
+//                            imageView.getDrawable().setColorFilter(argb, PorterDuff.Mode.SRC_ATOP);
+//                        }
+//                    }
+//                    , "testProperty", new ArgbEvaluator(), startArgb, endArgb);
+
+
 //            animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
 //
 //                @Override
 //                public void onAnimationUpdate(ValueAnimator animation) {
-//                    updateColorFilter(imageView);
+//                    updateColorFilter(imageView, (Integer)animation.getAnimatedValue());
 //                }
 //            });
+
+            animator.addUpdateListener(ColorFilterListener.createUsingAnimatedValue(imageView));
 //            animator.setDuration(duration).start();
         }
-        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
-            @Override
-            public void onAnimationUpdate(ValueAnimator animation) {
-                updateColorFilter(imageView);
-            }
-        });
         animator.setDuration(duration).start();
     }
+
+//    private void setTestProperty(int argb) {
+//        NLLog.i("setTestProperty", "argb : " + argb);
+//        imageView.getDrawable().setColorFilter(argb, PorterDuff.Mode.SRC_ATOP);
+//    }
 
     private static void updateColorFilter(ImageView imageView) {
         imageView.getDrawable().setColorFilter(imageView.getColorFilter());
     }
 
-    private static ObjectAnimator createObjectAnimatorBeforeLollipop(
-            Object target, String propertyName, int... values) {
-        ObjectAnimator animator
-                = ObjectAnimator.ofInt(target, propertyName, values);
-        animator.setEvaluator(new ArgbEvaluator());
-
-        return animator;
+    private static void updateColorFilter(ImageView imageView, int animatedValue) {
+        imageView.getDrawable().setColorFilter(animatedValue, PorterDuff.Mode.SRC_ATOP);
     }
 
     @TargetApi(Build.VERSION_CODES.LOLLIPOP)
@@ -76,28 +95,35 @@ public class ImageFilterAnimator {
         return ObjectAnimator.ofArgb(target, propertyName, values);
     }
 
+    private static class ColorFilterListener implements ValueAnimator.AnimatorUpdateListener {
+        private final ImageView mHeroImageView;
+        private boolean mUseAnimatedValue = false;
 
-    /**
-     * Copied from NineOldAndroids library.
-     */
-//    private static class ArgbEvaluator implements TypeEvaluator {
-//        public Object evaluate(float fraction, Object startValue, Object endValue) {
-//            int startInt = (Integer) startValue;
-//            int startA = (startInt >> 24);
-//            int startR = (startInt >> 16) & 0xff;
-//            int startG = (startInt >> 8) & 0xff;
-//            int startB = startInt & 0xff;
-//
-//            int endInt = (Integer) endValue;
-//            int endA = (endInt >> 24);
-//            int endR = (endInt >> 16) & 0xff;
-//            int endG = (endInt >> 8) & 0xff;
-//            int endB = endInt & 0xff;
-//
-//            return (int)((startA + (int)(fraction * (endA - startA))) << 24) |
-//                    (int)((startR + (int)(fraction * (endR - startR))) << 16) |
-//                    (int)((startG + (int)(fraction * (endG - startG))) << 8) |
-//                    (int)((startB + (int)(fraction * (endB - startB))));
-//        }
-//    }
+        private ColorFilterListener(ImageView hero) {
+            mHeroImageView = hero;
+        }
+
+        private ColorFilterListener(ImageView hero, boolean useAnimatedValue) {
+            mHeroImageView = hero;
+            mUseAnimatedValue = useAnimatedValue;
+        }
+
+        public static ColorFilterListener createUsingAnimatedValue(ImageView imageView) {
+            return new ColorFilterListener(imageView, true);
+        }
+
+        public static ColorFilterListener create(ImageView imageView) {
+            return new ColorFilterListener(imageView);
+        }
+
+        @Override
+        public void onAnimationUpdate(ValueAnimator valueAnimator) {
+            if (mUseAnimatedValue) {
+                int animatedValue = (Integer)valueAnimator.getAnimatedValue();
+                mHeroImageView.getDrawable().setColorFilter(animatedValue, PorterDuff.Mode.SRC_ATOP);
+            } else {
+                mHeroImageView.getDrawable().setColorFilter(mHeroImageView.getColorFilter());
+            }
+        }
+    }
 }
