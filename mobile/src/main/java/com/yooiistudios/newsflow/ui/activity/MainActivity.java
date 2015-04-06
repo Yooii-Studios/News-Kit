88cc5e1fa018872a080fb3fe702fb14cf774b7a1
Chris Jeong <crowjdh@gmail.com>
2015-04-06 15:35:15 +0900

    노티 임시로 구현해 메인->액션메뉴에 추가해둠

--- mobile/src/main/java/com/yooiistudios/newsflow/ui/activity/MainActivity.java
+++ mobile/src/main/java/com/yooiistudios/newsflow/ui/activity/MainActivity.java
@@ -60,6 +60,7 @@ import com.yooiistudios.newsflow.util.AdUtils;
 import com.yooiistudios.newsflow.util.AnalyticsUtils;
 import com.yooiistudios.newsflow.util.AppValidationChecker;
 import com.yooiistudios.newsflow.util.FacebookUtils;
+import com.yooiistudios.newsflow.util.NotificationUtils;
 import com.yooiistudios.newsflow.util.OnMainPanelEditModeEventListener;
 import com.yooiistudios.newsflow.util.ReviewUtils;
 
@@ -467,6 +468,7 @@ public class MainActivity extends ActionBarActivity
             subMenu.add(Menu.NONE, R.id.action_copy_db, 7, "Copy db to sdcard(Debug");
             subMenu.add(Menu.NONE, R.id.action_slow_anim, 8, "Slow Activity Transition(Debug)");
             subMenu.add(Menu.NONE, R.id.action_service_log, 9, "Show service log(Debug)");
+            subMenu.add(Menu.NONE, R.id.action_trigger_notification, 10, "Notification(Debug)");
         }
         MenuItemCompat.setShowAsAction(subMenu.getItem(), MenuItemCompat.SHOW_AS_ACTION_ALWAYS);
         return true;
@@ -510,6 +512,8 @@ public class MainActivity extends ActionBarActivity
             item.setChecked(!item.isChecked());
         } else if (id == R.id.action_service_log) {
             BackgroundServiceUtils.showDialog(this);
+        } else if (id == R.id.action_trigger_notification) {
+            NotificationUtils.issue(getApplicationContext());
         }
         return super.onOptionsItemSelected(item);
     }
