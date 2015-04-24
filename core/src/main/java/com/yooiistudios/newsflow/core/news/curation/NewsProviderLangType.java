ce8dd7785085c23ce013fe670648ece24c0142f5
Wooseong Kim <kwosu87@me.com>
2015-04-24 13:10:41 +0900

    이사님 새로 주신 기획으로 영어부터 수정

--- core/src/main/java/com/yooiistudios/newsflow/core/news/curation/NewsProviderLangType.java
+++ core/src/main/java/com/yooiistudios/newsflow/core/news/curation/NewsProviderLangType.java
@@ -15,21 +15,21 @@ public enum NewsProviderLangType {
     CHINESE_CN(3, R.raw.news_data_zh_cn, "简体中文"),
     CHINESE_TW(4, R.raw.news_data_zh_tw, "繁體中文"),
     GERMAN(5, R.raw.news_data_de, "Deutsch"),
-    DUTCH(6, R.raw.news_data_nl, "Nederlands"),
+    RUSSIAN(6, R.raw.news_data_ru, "Pусский"),
     PORTUGUESE(7, R.raw.news_data_pt, "Português"),
-    RUSSIAN(8, R.raw.news_data_ru, "Pусский"),
-    JAPANESE(9, R.raw.news_data_ja, "日本語"),
-    KOREAN(10, R.raw.news_data_ko, "한국어"),
-    TURKISH(11, R.raw.news_data_tr, "Türk"),
-    ITALIAN(12, R.raw.news_data_it, "Italiano"),
-    NORWEGIAN(13, R.raw.news_data_no, "Norsk"),
-    VIETNAMESE(14, R.raw.news_data_vi, "Tiếng Việt"),
-    SWEDISH(15, R.raw.news_data_sv, "Svenska"),
+    JAPANESE(8, R.raw.news_data_ja, "日本語"),
+    KOREAN(9, R.raw.news_data_ko, "한국어"),
+    TURKISH(10, R.raw.news_data_tr, "Türk"),
+    ITALIAN(11, R.raw.news_data_it, "Italiano"),
+    VIETNAMESE(12, R.raw.news_data_vi, "Tiếng Việt"),
+    SWEDISH(13, R.raw.news_data_sv, "Svenska"),
+    NORWEGIAN(14, R.raw.news_data_no, "Norsk"),
+    FINNISH(15, R.raw.news_data_fi, "Suomi"),
     DANISH(16, R.raw.news_data_da, "Dansk"),
-    FINNISH(17, R.raw.news_data_fi, "Suomi"),
-    INDONESIAN(18, R.raw.news_data_id, "Indonesia"),
+    DUTCH(17, R.raw.news_data_nl, "Nederlands"),
+    THAI(18, R.raw.news_data_th, "ไทย"),
     MALAY(19, R.raw.news_data_ms, "Malay"),
-    THAI(20, R.raw.news_data_th, "ไทย");
+    INDONESIAN(20, R.raw.news_data_id, "Indonesia");
 
     private int mIndex;
     private int mResourceId;
@@ -43,27 +43,27 @@ public enum NewsProviderLangType {
 
     public static NewsProviderLangType valueOf(int index) {
         switch (index) {
-            case 0: return ENGLISH;
-            case 1: return SPANISH;
-            case 2: return FRENCH;
-            case 3: return CHINESE_CN;
-            case 4: return CHINESE_TW;
-            case 5: return GERMAN;
-            case 6: return DUTCH;
-            case 7: return PORTUGUESE;
-            case 8: return RUSSIAN;
-            case 9: return JAPANESE;
-            case 10: return KOREAN;
-            case 11: return TURKISH;
-            case 12: return ITALIAN;
-            case 13: return NORWEGIAN;
-            case 14: return VIETNAMESE;
-            case 15: return SWEDISH;
+            case 0:  return ENGLISH;
+            case 1:  return SPANISH;
+            case 2:  return FRENCH;
+            case 3:  return CHINESE_CN;
+            case 4:  return CHINESE_TW;
+            case 5:  return GERMAN;
+            case 6:  return RUSSIAN;
+            case 7:  return PORTUGUESE;
+            case 8:  return JAPANESE;
+            case 9:  return KOREAN;
+            case 10: return TURKISH;
+            case 11: return ITALIAN;
+            case 12: return VIETNAMESE;
+            case 13: return SWEDISH;
+            case 14: return NORWEGIAN;
+            case 15: return FINNISH;
             case 16: return DANISH;
-            case 17: return FINNISH;
-            case 18: return INDONESIAN;
+            case 17: return DUTCH;
+            case 18: return THAI;
             case 19: return MALAY;
-            case 20: return THAI;
+            case 20: return INDONESIAN;
             default: return ENGLISH;
         }
     }
