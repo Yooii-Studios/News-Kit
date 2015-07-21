e76c9a140bc06c2ec41eb14b747551b0c1c19e1d
Wooseong Kim <kwosu87@me.com>
2015-07-21 23:26:39 +0900

    페이스북 유이스튜디오 페이지 로딩 로직 변경되서 수정

--- mobile/src/main/java/com/yooiistudios/newskit/util/FacebookUtils.java
+++ mobile/src/main/java/com/yooiistudios/newskit/util/FacebookUtils.java
@@ -2,7 +2,6 @@ package com.yooiistudios.newskit.util;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.net.Uri;
 
 /**
@@ -12,18 +11,15 @@ import android.net.Uri;
  *  페이스북의 유이스튜디오 페이지 링크를 열어주는 유틸
  */
 public class FacebookUtils {
-    private static final String LINK_APP_PREFIX = "fb://profile/";
-    private static final String FB_YOOII_ID = "652380814790935";
+//    private static final String FB_YOOII_ID = "652380814790935";
+    private static final String FACEBOOK_URL = "https://www.facebook.com/YooiiMooii";
 
     private FacebookUtils() { throw new AssertionError("You can't create this class!"); }
 
     public static void openYooiiPage(Context context) {
         try {
-            PackageManager packageManager = context.getPackageManager();
-            if (packageManager != null) {
-                packageManager.getPackageInfo("com.facebook.katana", 0);
-            }
-            context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(LINK_APP_PREFIX + FB_YOOII_ID)));
+            Uri uri = Uri.parse("fb://facewebmodal/f?href=" + FACEBOOK_URL);
+            context.startActivity(new Intent(Intent.ACTION_VIEW, uri));
         } catch (Exception e) {
             context.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/YooiiMooii")));
         }
