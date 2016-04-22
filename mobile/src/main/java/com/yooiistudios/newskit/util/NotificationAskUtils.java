2f5312a03b99efb47b7e24f06f9496cf7305808b
Chris Jeong <crowjdh@gmail.com>
2016-04-22 14:53:07 +0900

    앱 최초 구동시 알림 수신 여부 선택 다이얼로그를 back button으로 dismiss할 수 있도록 변경(피쳐드 요구사항)

--- mobile/src/main/java/com/yooiistudios/newskit/util/NotificationAskUtils.java
+++ mobile/src/main/java/com/yooiistudios/newskit/util/NotificationAskUtils.java
@@ -28,7 +28,6 @@ public class NotificationAskUtils {
                 })
                 .build();
 
-        dialog.setCancelable(false);
         dialog.setCanceledOnTouchOutside(false);
         dialog.show();
     }
